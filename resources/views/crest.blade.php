b9e86df83a14d197b3867d660d59b82b2ad1fff3
Lothindil <aduterre@gmail.com>
2023-10-06 00:12:23 +0200

    base css

--- resources/views/crest.blade.php
+++ resources/views/crest.blade.php
@@ -1,13 +1,22 @@
 @extends('template')
 @section('contenu')
     <h1>Blason aléatoire</h1>
-    <p>De {{$couleur_champs->nom}} ({{$couleur_champs->hexadecimal}})
-    @if($aff_meuble)
-    au {{$meuble->nom}} de {{$couleur_meuble->nom}} ({{$couleur_meuble->hexadecimal}})
-    @else
-    plein
-    @endif
-    </p>
-    <img src='{{$blason}}' />
+<div class='flex-vertical'>
+    <div class='flex-vertical'>
+        <p>De {{$couleur_champs->nom}} ({{$couleur_champs->hexadecimal}})
+        @if($aff_meuble)
+        au {{$meuble->nom}} de {{$couleur_meuble->nom}} ({{$couleur_meuble->hexadecimal}})
+        @else
+        plein
+        @endif
+        </p>
+    </div>
+    <div class='flex-horizontal'>
+        <img src='{{$blason}}' />
+    <div class='flex-vertical'>
+    
+    </div>
+    </div>
+</div>
     
 @endsection
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
+    <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css" />
 </head>
 <body>
     @yield('contenu')
